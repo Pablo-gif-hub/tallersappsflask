4caba75c53133bbfa6da6d98ab11393b7f9af878
Marco_Cs <marco.copa@uab.edu.bo>
2026-03-01 23:53:57 -0400

    actualizacion

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from extension import db
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret1234'
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/db_tareas"
